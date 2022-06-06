dd677b10b5227ef631f6fa4444dd5ee5c095b084
rachell <dazzlemevanity@gmail.com>
2022-06-06 11:03:01 -0400

    commit tree c6653ddad7d160a361d9d1bbb2fbb193315e6dac

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// function superbowlWin(element,index,array){
+// const find = [
+//     {year:}
+// ]
+// }
+
+function superbowlWin(array){
+const found = array.find(element => (element.result === 'W')) 
+if (found === undefined){
+    return undefined
+}    
+else if (found.result === 'W'){
+return found.year
+}
+}
